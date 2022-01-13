63dafcc75a9870bc7b9befadadddaa4f891c10a1
[ShinJongUng] <[whddnd0728@gmail.com]>
2022-01-13 21:36:33 +0900

    readme

--- lib/mainStyle.dart
+++ lib/mainStyle.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 var mainTheme = ThemeData(
-  appBarTheme: AppBarTheme(
-    color: Color(0xffFBEAEB),
-    titleTextStyle:TextStyle(fontFamily: "NanumSquareRoundEB", color:  Color(0xff2E3C7E), fontSize: 25),
-  ),
-  bottomNavigationBarTheme: BottomNavigationBarThemeData(
-    backgroundColor: Color(0xff2E3C7E),
-    selectedItemColor: Colors.white,
-    unselectedItemColor: Colors.white,
-  )
+    appBarTheme: AppBarTheme(
+      color: Color(0xffFBEAEB),
+      titleTextStyle:TextStyle(fontFamily: "NanumSquareRoundEB", color:  Color(0xff2E3C7E), fontSize: 25),
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: Color(0xff2E3C7E),
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white,
+    )
 );
