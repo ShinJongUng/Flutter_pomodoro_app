da776c857ace15e630e061ba5f5c1466e5bd7144
[ShinJongUng] <[whddnd0728@gmail.com]>
2022-01-20 15:43:58 +0900

    차트 재생성

--- lib/indexPage/first_page.dart
+++ lib/indexPage/first_page.dart
@@ -2,64 +2,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-//dart 시간 chart 패키지
-import 'package:time_chart/time_chart.dart';
+//dart chart 패키지
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-class analyzePage extends StatelessWidget {
+class analyzePage extends StatefulWidget {
   const analyzePage({Key? key}) : super(key: key);
 
+  @override
+  State<analyzePage> createState() => _analyzePageState();
+}
+
+
+class _analyzePageState extends State<analyzePage> {
+  late TooltipBehavior _tooltipBehavior;
+  late ZoomPanBehavior _zoomPanBehavior;
+  List<ChartData> chartData = [ChartData(3,23), ChartData(32,23),];
+  ChartSeriesController? _chartSeriesController;
 
   @override
+  void initState() {
+    _zoomPanBehavior = ZoomPanBehavior(
+      // Enables pinch zooming
+        enableMouseWheelZooming : true,
+        enablePinching: true,
+    );
+    _tooltipBehavior =  TooltipBehavior(enable: true);
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+
   Widget build(BuildContext context) {
-    final sizedBox = const SizedBox(height: 16);
-
-    final data = [ DateTimeRange(
-      start: DateTime(2021,2,24,23,15),
-      end: DateTime(2021,2,25,7,30),
-    ),];
-
 
     return SizedBox.expand(
-      child: SingleChildScrollView(
-        child: Container(
-          color: Color(0xffffffff),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-                children: [
-                  const Text('Weekly time chart'),
-                  TimeChart(
-                    data: data,
-                    viewMode: ViewMode.weekly,
-                  ),
-                  sizedBox,
-                  const Text('Monthly time chart'),
-                  TimeChart(
-                    data: data,
-                    viewMode: ViewMode.monthly,
-                  ),
-                  sizedBox,
-                  const Text('Weekly amount chart'),
-                  TimeChart(
-                    data: data,
-                    chartType: ChartType.amount,
-                    viewMode: ViewMode.weekly,
-                    barColor: Colors.deepPurple,
-                  ),
-                  sizedBox,
-                  const Text('Monthly amount chart'),
-                  TimeChart(
-                    data: data,
-                    chartType: ChartType.amount,
-                    viewMode: ViewMode.monthly,
-                    barColor: Colors.deepPurple,
-                  ),
-                ],
-              ),
-            ),
-        ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton(onPressed: (){}, child: Text('dd')),
+              TextButton(onPressed: (){
+                setState(() {
+                  chartData.add(ChartData(322,23));
+                  _chartSeriesController?.updateDataSource(
+                    addedDataIndexes: <int>[chartData.length - 1],
+                    removedDataIndexes: <int>[0],
+                  );
+                });
 
-      ),
+              }, child: Text('dd'))
+            ],
+          ),
+          SfCartesianChart(
+            // Initialize category axis
+              title: ChartTitle(text: '지난 집중 시간 기록'),
+              zoomPanBehavior: _zoomPanBehavior,
+              tooltipBehavior: _tooltipBehavior,
+              primaryXAxis: CategoryAxis(),
+              series: <ChartSeries>[
+                // Initialize line series
+                LineSeries<ChartData, num>(
+                    name:'날짜',
+                    enableTooltip: true,
+                    dataSource: chartData,
+                    xValueMapper: (ChartData days, _) => days.day,
+                    yValueMapper: (ChartData days, _) => days.sales,
+                    dataLabelSettings:DataLabelSettings(isVisible : true)
+                )
+              ]
+          )
+        ],
+      )
     );
   }
+}
+
+class ChartData {
+  ChartData(this.day, this.sales);
+  final num day;
+  final double? sales;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 //dart 편의 패키지
-import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:async';
 import 'notification.dart';
